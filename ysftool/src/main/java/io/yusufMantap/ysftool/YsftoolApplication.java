0e3cc2af45e26c45890db641b482a2fd9c8736af
yusuf1998 <yusufhabibi1998@gmail.com>
2020-05-12 16:51:31 +0700

    add folder

---
+++ ysftool/src/main/java/io/yusufMantap/ysftool/YsftoolApplication.java
@@ -0,0 +1,13 @@
+package io.yusufMantap.ysftool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsftoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsftoolApplication.class, args);
+	}
+
+}
---
+++ ysftool/src/test/java/io/yusufMantap/ysftool/YsftoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yusufMantap.ysftool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsftoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
